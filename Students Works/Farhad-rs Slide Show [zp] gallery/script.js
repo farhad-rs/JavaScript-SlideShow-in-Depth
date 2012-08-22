bb46da7950eaee9a7b714fe471c6ae08a5d72e93
farhad-rs <farhad.raeesi@gmail.com>
2012-08-22 15:26:24 +0430

    Add JS code in script.js

--- Students Works/Farhad-rs Slide Show [zp] gallery/script.js
+++ Students Works/Farhad-rs Slide Show [zp] gallery/script.js
@@ -1,4 +1,16 @@
 Math.rnd = Math.random;
-Math.random = function (a,b){
-	return(!a && !b) ? Math.rnd() : Math.floor(Math.rnd()*(b-a+1))+a;	
+Math.random = function (a,b) {
+	return (!a && !b) ? Math.rnd() : Math.floor(Math.rnd()*(b-a+1))+a;	
 }
+$(function() {
+	var pics = $(".slideshow a");
+	MaxWidth = window.innerWidth;
+	MaxHeight = window.innerWidth;
+	pics.each(function() {
+		$(this).animate({
+			left:Math.random(0,MaxWidth-200),
+			top:Math.random(0,MaxHeight-200),
+			'-webkit-transform':'rotate('+Math.random(-75,75)+'deg)'
+		},3500,'cubic-bezier(.05,.1,.5,.95)');
+	});
+});
