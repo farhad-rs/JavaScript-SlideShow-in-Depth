0a8740db49de1aef8112d0ca850e9dd3bca4f3ff
farhad-rs <farhad.raeesi@gmail.com>
2012-08-22 14:40:48 +0430

    Add php code in index.php

--- Students Works/Farhad-rs Slide Show [zp] gallery/index.php
+++ Students Works/Farhad-rs Slide Show [zp] gallery/index.php
@@ -9,5 +9,20 @@
 	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
+	<div class="container">
+		<div class="pic">
+			<?php
+				$image_path = "../../images";
+				$image = scandir($image_path);
+				foreach($image as $pic){
+					$pic_arr = explode('.',$pic);
+					$pic_type = strtolower(end($pic_arr));
+					if ($pic_type == "jpg"){
+						echo "<a href=\"$image_path/$pic\"><img src=\"$image_path/farhad-image/gallery/small/$pic\" alt=\"New and nice pictures of 2012\" /></a>";	
+					}
+				}
+			?>
+		</div>
+	</div>
 </body>
 </html>
